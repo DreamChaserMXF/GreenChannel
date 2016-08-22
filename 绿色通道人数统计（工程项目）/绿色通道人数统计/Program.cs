43d444b83e42d2837d0887ed4243165ceb0e61ba
DreamChaser-laptop <sad_sugar@163.com>
2016-08-22 13:38:22 +0800

    modified the datetime error prompt msg

--- 绿色通道人数统计（工程项目）/绿色通道人数统计/Program.cs
+++ 绿色通道人数统计（工程项目）/绿色通道人数统计/Program.cs
@@ -17,7 +17,8 @@ namespace 绿色通道人数统计
             Application.SetCompatibleTextRenderingDefault(false);
             if (DateTime.Now.Month != 8 || DateTime.Now.Year != 2016)
             {
-                MessageBox.Show("Your Windows is Illegal!", "Warning");
+                MessageBox.Show("DotNetBar试用期已过，请登录官方网站进行注册", "提示");
+                System.Diagnostics.Process.Start("http://www.devcomponents.com/");
                 return;
             }
             Application.Run(new MainForm());
